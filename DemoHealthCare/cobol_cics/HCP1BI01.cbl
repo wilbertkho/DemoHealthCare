f56c6eec4fffc7034e1a3a0c5aa1603fe2541546
wilbert <wilbert@us.ibm.com>
2020-11-12 08:47:02 -0800

    Changed header 11/12/2020 8:46 wk

--- DemoHealthCare/cobol_cics/HCP1BI01.cbl
+++ DemoHealthCare/cobol_cics/HCP1BI01.cbl
@@ -1,4 +1,4 @@
-      *   Inquire Patient    - Nov 12 2020 08:12
+      *   Inquire Patient    - Nov 12 2020 08:46
       *   change  obtain Patient's details from database.              *
       * Patient Inquire Business logic                                 *
       ******************************************************************
